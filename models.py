d489c415cd111101ac6ae73afdd92c25d531b656
yosefbs <yosef.b.s@outlook.com>
2019-11-14 18:59:48 +0200

    Add files via upload

--- models.py
+++ models.py
@@ -37,50 +37,60 @@ class Borad:
     def moveQueenToNode(self, queenId, rowId, colId):
         self.leaveNode(queenId)
         self.queens[queenId].setLocation(rowId, colId)
-        self.setAttackInfo(queenId)
+        self.calcAttackableNodes(queenId)
 
-    def setAttackInfo(self, queenId):
+    def calcAttackableNodes(self, queenId):
         q = self.queens[queenId]
         r = q.location[0]
         c = q.location[1]
         for i in range(0, self.n):
-            self.nodes[(r, i)].addAttacker(q)
+            self.setNodeAttacker(r,i,q)
             if i != r:
-                self.nodes[(i, c)].addAttacker(q)
+                self.setNodeAttacker(i, c, q)
 
         tempR = r + 1
         tempC = c + 1
         while (tempR < self.n) and (tempC < self.n):
-            self.nodes[(tempR, tempC)].addAttacker(q)
+            self.setNodeAttacker(tempR, tempC, q)
             tempR += 1
             tempC += 1
 
         tempR = r - 1
         tempC = c - 1
         while (tempR > -1) and (tempC > -1):
-            self.nodes[(tempR, tempC)].addAttacker(q)
+            self.setNodeAttacker(tempR, tempC, q)
             tempR -= 1
             tempC -= 1
 
         tempR = r + 1
         tempC = c - 1
         while (tempR < self.n) and (tempC > -1):
-            self.nodes[(tempR, tempC)].addAttacker(q)
+            self.setNodeAttacker(tempR, tempC, q)
             tempR += 1
             tempC -= 1
 
         tempR = r - 1
         tempC = c + 1
         while (tempR > -1) and (tempC < self.n):
-            self.nodes[(tempR, tempC)].addAttacker(q)
+            self.setNodeAttacker(tempR, tempC, q)
             tempR -= 1
             tempC += 1
+
+    def setNodeAttacker(self, row, col, queen):
+        node = self.nodes[(row, col)]
+        node.addAttacker(queen)
+        if node in self.freeNodes:
+            self.freeNodes.remove(node)
+
+
 
     def leaveNode(self, queenId):
         q = self.queens[queenId]
         if q.location == (-1, -1):
             return
 
+        for node in q.uniqueAttackableNode:
+            self.freeNodes.append(node)
         q.uniqueAttackableNode = []
         r = q.location[0]
         c = q.location[1]

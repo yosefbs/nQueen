3f9f19feab6b517533e71bfa074404d1d6056d33
yosef.b.s@outlook.com <yosef.b.s@outlook.com>
2019-11-19 10:34:54 +0200

    commm

--- main.py
+++ main.py
@@ -21,28 +21,26 @@ def debug():
 
 
 def runMultiGames():
-    numOfWinReq = 1000
-    for i in range(12,27,1):
-        print(i)
-        maxMoves = i
-        winCounter = 0
-        playCounter = 0
-        totalMoves = 0
-        totalWinGamesMoves = 0
-        while winCounter < numOfWinReq:
-            b = Board(8)
-            p = Player(b,maxMoves)
-            playCounter += 1
-            win = p.play()
-            if win:
-                totalWinGamesMoves += b.moveCounter
-                winCounter += 1
-               # b.printBorad()
-            totalMoves += b.moveCounter
-        print()
-        print('wins: ' + str(winCounter) + '/ play: ' + str(playCounter))
-        print(totalMoves / numOfWinReq)
-        print(totalWinGamesMoves / numOfWinReq)
+    numOfWinReq = 100
+    maxMoves = 15
+    winCounter = 0
+    playCounter = 0
+    totalMoves = 0
+    totalWinGamesMoves = 0
+    while winCounter < numOfWinReq:
+        b = Board(8)
+        p = Player(b,maxMoves)
+        playCounter += 1
+        win = p.play()
+        if win:
+            totalWinGamesMoves += b.moveCounter
+            winCounter += 1
+           # b.printBorad()
+        totalMoves += b.moveCounter
+    print()
+    print('wins: ' + str(winCounter) + '/ play: ' + str(playCounter))
+    print(totalMoves / numOfWinReq)
+    print(totalWinGamesMoves / numOfWinReq)
 
 
 if __name__ == '__main__':
